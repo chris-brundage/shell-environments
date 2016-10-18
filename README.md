d705f14d884a7c698f2752ed8ee013b9db736924
Chris Brundage <christopher.m.brundage@gmail.com>
2016-10-17 21:18:02 -0500

    Reorganizing the repo into storage of bashrc and powershell profile.ps1

---
+++ README.md
@@ -0,0 +1,1 @@
+My personal bashrc and profile.ps1
--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,19 @@
+# Rudimentary replacement for bash_history
 $HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
 Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFilePath } | out-null
 if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
-# if you don't already have this configured...
+
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-$DownloadsFolder = (Get-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' -Name '{374DE290-123F-4565-9164-39C4925E467B}').'{374DE290-123F-4565-9164-39C4925E467B}'
+
+# Add special folders as Powershell drives
 $DocumentsFolder = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::MyDocuments)
 $DesktopFolder = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+
+# It is mind boggling that System.Environment.SpecialFolder doesn't have the Downloads folder in it.
+# https://msdn.microsoft.com/library/dd378457.aspx
+$DownloadsFolder = (Get-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders' -Name '{374DE290-123F-4565-9164-39C4925E467B}').'{374DE290-123F-4565-9164-39C4925E467B}'
 
 New-PSDrive -Name Downloads -PSProvider FileSystem -Root $DownloadsFolder | Out-Null
 New-PSDrive -Name Documents -PSProvider FileSystem -Root $DocumentsFolder | Out-Null
